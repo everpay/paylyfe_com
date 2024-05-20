0b0eaf6ecd50cc92adfb55e4e58c1f0edb4710f6
Elektropay <richard.r@everpayinc.com>
2024-05-20 07:00:53 -0500

    Update CTA.jsx

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -2,7 +2,7 @@ import styles from "../style";
 import Button from "./Button";
 
 const CTA = () => (
-  <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient rounded-[20px] box-shadow`}>
+  <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} mt-5 mb-10 sm:flex-row flex-col bg-black-gradient rounded-[20px] box-shadow`}>
     <div className="flex-1 flex flex-col">
       <h2 className={styles.heading2}>Let’s try our service now!</h2>
       <p className={`${styles.paragraph2} max-w-[470px] mt-5`}>
