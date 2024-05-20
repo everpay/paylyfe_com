4b0e128ba2a8f1827371ba58e8299a91e2646a69
Elektropay <richard.r@everpayinc.com>
2024-05-20 01:16:30 -0500

    Update CTA.jsx

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,9 +3,9 @@ import Button from "./Button";
 
 const CTA = () => (
   <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 text-white rounded-[20px] box-shadow`}>
-    <div className="flex-1 flex flex-col text-white">
-      <h2 className={styles.heading2}`}>Let’s try our service now!</h2>
-      <p className={`${styles.paragraph} max-w-[470px]`}>
+    <div className="flex-1 flex flex-col">
+      <h2 className={styles.heading2}>Let’s try our service now!</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
         Everything you need to accept card payments and grow your business
         anywhere on the planet.
       </p>
